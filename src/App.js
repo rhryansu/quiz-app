7452852322cbb7e00a9f4dd2710196d807096843
Ryan-Win <rh.ryan.su@gmail.com>
2022-05-10 03:33:25 +1000

    modify quiz questions

--- src/App.js
+++ src/App.js
@@ -4,21 +4,42 @@ export default function App() {
 	
 	const questions = [
 		{
-			questionText: 'When you work in the field, do you work most of the time inside a large machine(large harvester,etc) or in the field?',
+			questionText: 'When you work in the field, do you work most of the time inside a plant greenhouse or in the field?',
 			answerOptions: [
-				{ answerText: 'In a large machine', point: 10 },
+				{ answerText: 'In a plant greenhouse', point: 10 },
 				{ answerText: 'In the field', point: 0 },
 			],
 		},
 		{
-			questionText: 'Does your work require you to wear a mask or other item that covers your lips?',
+			questionText: 'Do you have a habit of wearing a hat when you work outside?',
 			answerOptions: [
 				{ answerText: 'Yes', point: 10 },
 				{ answerText: 'No', point: 0 },
 			],
 		},
 		{
-			questionText: 'Do you need to fly frequently?',
+			questionText: 'Do you have a habit of wearing long-sleeve clothes when you work outside?',
+			answerOptions: [
+				{ answerText: 'Yes', point: 10 },
+				{ answerText: 'No', point: 0 },
+			],
+		},
+		{
+			questionText: 'Do you have a habit of wearing a pair of gloves when you work outside?',
+			answerOptions: [
+				{ answerText: 'Yes', point: 10 },
+				{ answerText: 'No', point: 0 },
+			],
+		},
+		{
+			questionText: 'Do you have a habit of wearing a silk scarf when you work outside?',
+			answerOptions: [
+				{ answerText: 'Yes', point: 10 },
+				{ answerText: 'No', point: 0 },
+			],
+		},
+		{
+			questionText: 'Do you have a habit of using sunscreen when you work outside?',
 			answerOptions: [
 				{ answerText: 'Yes', point: 10 },
 				{ answerText: 'No', point: 0 },
@@ -27,25 +48,31 @@ export default function App() {
 		{
 			questionText: 'Do you have frequent exposure to water, such as manually watering crops?',
 			answerOptions: [
-				{ answerText: 'Yes', point: 10 },
-				{ answerText: 'No', point: 0 },
+				{ answerText: 'Yes', point: 0 },
+				{ answerText: 'No', point: 10 },
 			],
 		},
 		{
 			questionText: 'How many hours do you need to be exposed to the sun?',
 			answerOptions: [
-				{ answerText: 'Barely not', point: 20 },
-				{ answerText: 'Casually walk in the sun', point: 15 },
-				{ answerText: 'Might need to walk in more than 30 minutes', point: 10 },
-				{ answerText: 'For a long time(more than 30 minutes)', point: 5 },
+				{ answerText: '0 hour', point: 20 },
+				{ answerText: '0-0.5 hours', point: 15 },
+				{ answerText: '0.5-1 hours', point: 10 },
+				{ answerText: '> 1 hours', point: 5 },
 			],
 		},
 		{
-			questionText: 'whether your skin is experiencing heat, erythema, burning, pain, peeling or itching? If yes, How many days have you had the above symptoms?',
+			questionText: 'Has your skin ever experienced heat, erythema, burning, pain, peeling or itching?',
 			answerOptions: [
-				{ answerText: 'After 3 days and symptoms are getting worse', point: 20 },
-				{ answerText: 'Below 3 days', point: 20 },
+				{ answerText: 'Yes', point: 20 },
 				{ answerText: 'No', point: 0 },
+			],
+		},
+		{
+			questionText: 'How many days have you had the skin symptoms, which are heat, erythema, burning, pain, peeling or itching?',
+			answerOptions: [
+				{ answerText: '> 3 days', point: 0 },
+				{ answerText: '< 3 days', point: 5 },
 			],
 		},
 	];
@@ -86,15 +113,12 @@ export default function App() {
 
 	const adviceHandler = (score) => {
 		console.log("score in handler is " + score);
-		if (score > 60) {
-			setAdvice(`Congratulations! It looks like you are one of those people whose work and lifestyle habits don't make you too susceptible to skin cancer! It is very rare for you to have such a good working environment in rural Victoria where the incidence of skin cancer is very high. Congratulations and hope you will stay free of skin cancer ever!
-			However, here are a few suggestions for your work and living environment and habits that we hope you will read carefully and use. This way, you will have a good chance of keeping your skin healthy!`);
-		} else if (score > 40 && score < 61) {
-			setAdvice(`Ah-oh! It looks like you have some habits and work environments that expose you to the threat of UV rays. While it may not seem like such a threat is serious right now, you do need to be fully aware of the importance of sun protection. After all, staying healthy is extremely critical to you and your familes!
-			Here are a few suggestions for your work and living environment and habits that we hope you will read carefully and use. This way, you will have a good chance of keeping your skin healthy!`);
+		if (score > 80) {
+			setAdvice(`Congratulations! It looks like you are one of those people whose work and lifestyle habits don't make you too susceptible to skin cancer! It is very rare for you to have such a good working environment in rural Victoria where the incidence of skin cancer is very high. Congratulations and hope you will stay free of skin cancer ever! However, here are a few suggestions for your work and living environment and habits that we hope you will read carefully and use. This way, you will have a good chance of keeping your skin healthy!`);
+		} else if (score > 40 && score <= 80) {
+			setAdvice(`Ah-oh! It looks like you have some habits and work environments that expose you to the threat of UV rays. While it may not seem like such a threat is serious right now, you do need to be fully aware of the importance of sun protection. After all, staying healthy is extremely critical to you and your families! Here are a few suggestions for your work and living environment and habits that we hope you will read carefully and use. This way, you will have a good chance of keeping your skin healthy!`);
 		} else {
-			setAdvice(`Ooops! It looks like your work environment and lifestyle habits expose you to the threat of UV rays on a regular basis! Such a threat can put you at a much higher risk of developing skin cancer. But don't worry, we'll give you a series of tips that, if you really take them seriously and keep them up, you'll be well away from the risk of developing the disease.
-			Take a look at the advice below! Do what you have to do to maintain a healthy skin in the future!`);
+			setAdvice(`Ooops! It looks like your work environment and lifestyle habits expose you to the threat of UV rays on a regular basis! Such a threat can put you at a much higher risk of developing skin cancer. But don't worry, we'll give you a series of tips that, if you really take them seriously and keep them up, you'll be well away from the risk of developing the disease. Take a look at the advice below! Do what you have to do to maintain healthy skin in the future.`);
 		}
 	}
 
