457d90bd9d88d9c3882828d1aa1cabe0099747fd
Ruihua Ryan Su <rh.ryan.su@gmail.com>
2022-05-16 20:35:29 +1000

    v2.0 Redesign Structure

--- src/App.js
+++ src/App.js
@@ -6,73 +6,67 @@ export default function App() {
 		{
 			questionText: 'When you work in the field, do you work most of the time inside a plant greenhouse or in the field?',
 			answerOptions: [
-				{ answerText: 'In a plant greenhouse', point: 10 },
-				{ answerText: 'In the field', point: 0 },
+				{ answerText: 'In a plant greenhouse', point: 10, tips: 'Wear Sunglasses in CAT 2 (Transmission ratio from 18% to 43%) ' },
+				{ answerText: 'In the field', point: 0, tips: 'Wear Sunglasses in CAT 3 (Transmission ratio from 8% to 18%) or CAT 4 (Transmission ratio from 3% - 8%) ' },
 			],
 		},
 		{
 			questionText: 'Do you have a habit of wearing a hat when you work outside?',
 			answerOptions: [
-				{ answerText: 'Yes', point: 10 },
-				{ answerText: 'No', point: 0 },
+				{ answerText: 'Yes', point: 10, tips: 'Keep wearing a hap or other protective item. ' },
+				{ answerText: 'No', point: 0, tips: 'Wear a hap when you are outside, especially in a sunny day. '},
 			],
 		},
 		{
 			questionText: 'Do you have a habit of wearing long-sleeve clothes when you work outside?',
 			answerOptions: [
-				{ answerText: 'Yes', point: 10 },
-				{ answerText: 'No', point: 0 },
+				{ answerText: 'Yes', point: 10, tips: 'Keep wearing long-sleeve clothes. ' },
+				{ answerText: 'No', point: 0, tips: 'Wear long-sleeve clothes when you are outside, especially in a sunny day. ' },
 			],
 		},
 		{
 			questionText: 'Do you have a habit of wearing a pair of gloves when you work outside?',
 			answerOptions: [
-				{ answerText: 'Yes', point: 10 },
-				{ answerText: 'No', point: 0 },
+				{ answerText: 'Yes', point: 10, tips: 'Keep wearing a pair of gloves. '},
+				{ answerText: 'No', point: 0, tips: 'Wear a pair of gloves when you are outside, especially in a sunny day. ' },
 			],
 		},
 		{
 			questionText: 'Do you have a habit of wearing a silk scarf when you work outside?',
 			answerOptions: [
-				{ answerText: 'Yes', point: 10 },
-				{ answerText: 'No', point: 0 },
+				{ answerText: 'Yes', point: 10, tips: 'Keep wearing a silk scarf. ' },
+				{ answerText: 'No', point: 0, tips: 'Wear a silk scarf to protect your neck when you are outside, especially in a sunny day. ' },
 			],
 		},
 		{
 			questionText: 'Do you have a habit of using sunscreen when you work outside?',
 			answerOptions: [
-				{ answerText: 'Yes', point: 10 },
-				{ answerText: 'No', point: 0 },
+				{ answerText: 'Yes', point: 10, tips:'Keep using sunscreen. ' },
+				{ answerText: 'No', point: 0, tips: 'Using sun screen when you are outside, especially in a sunny day. ' },
 			],
 		},
 		{
 			questionText: 'Do you have frequent exposure to water, such as manually watering crops?',
 			answerOptions: [
-				{ answerText: 'Yes', point: 0 },
-				{ answerText: 'No', point: 10 },
+				{ answerText: 'Yes', point: 0, tips: 'Choose sunscreen with water resistant (If a product is labelled with water resistant: 80 minutes, it means you need to reapply in after 80 minutes). ' },
+				{ answerText: 'No', point: 10, tips: 'Choose normal sunscreen will be enough. ' },
 			],
 		},
 		{
 			questionText: 'How many hours do you need to be exposed to the sun?',
 			answerOptions: [
-				{ answerText: '0 hour', point: 20 },
-				{ answerText: '0-0.5 hours', point: 15 },
-				{ answerText: '0.5-1 hours', point: 10 },
-				{ answerText: '> 1 hours', point: 5 },
+				{ answerText: '0 hour', point: 20, tips: 'Use sunscreen with SPF 6-14 or PA+/PA++ '},
+				{ answerText: '0-0.5 hours', point: 15, tips: 'Use sunscreen with SPF 15-29 or PA+/PA++ ' },
+				{ answerText: '0.5-1 hours', point: 10, tips: 'Use sunscreen with SPF 30-50 or PA+/PA+++ ' },
+				{ answerText: '> 1 hours', point: 5, tips: 'Use sunscreen with SPF 50+ or PA+++ ' },
 			],
 		},
 		{
-			questionText: 'Has your skin ever experienced heat, erythema, burning, pain, peeling or itching?',
+			questionText: 'Has your skin ever experienced heat, erythema, burning, pain, peeling or itching? If yes, how many days is it lasting?',
 			answerOptions: [
-				{ answerText: 'Yes', point: 20 },
-				{ answerText: 'No', point: 0 },
-			],
-		},
-		{
-			questionText: 'How many days have you had the skin symptoms, which are heat, erythema, burning, pain, peeling or itching?',
-			answerOptions: [
-				{ answerText: '> 3 days', point: 0 },
-				{ answerText: '< 3 days', point: 5 },
+				{ answerText: '> 3 days', point: 20, tips: 'Go to hospital when you have had symptoms of sunburn for more than three days!' },
+				{ answerText: '< 3 days', point: 25, tips: 'Use cleansing products with the ingredient of Decyl glucoside, Cocoyl Glutamate or  Sodium Lauryl Glucose Carboxylate, aloe vera gel, product with Ectoine (tetrahydro methyl pyrimidine carboxylic acid), Serum with blue copper peptides.' },
+				{ answerText: 'No', point: 0, tips: 'No need for sunburn repair and also continue good sun protection measures.'},
 			],
 		},
 	];
@@ -83,8 +77,10 @@ export default function App() {
 	const [showScore, setShowScore] = useState(false);
 	const [score, setScore] = useState(0);
 	const [advice, setAdvice] = useState('');
+	const [suggestions, setSuggestions] = useState('');
 
-	const handleAnswerButtonClick = (point) => {
+
+	const handleAnswerButtonClick = (point, tips) => {
 
 		const nextQuestion = currentQuestion + 1;
 		const nextQuestionIndex = currentQuestionIndex + 1;
@@ -94,6 +90,9 @@ export default function App() {
 			let localScore = score;	
 			localScore += point;
 			setScore(localScore);
+			let localSuggestions = suggestions;
+			localSuggestions = localSuggestions + tips
+			setSuggestions(localSuggestions);
 
 		console.log("the score in function aferwards is " + score);
 		console.log("the point passed to the function is " + point);
@@ -103,7 +102,8 @@ export default function App() {
 			setCurrentQuestionIndex(nextQuestionIndex);
 		} else {
 			setShowScore(true);
-			adviceHandler(score);
+			adviceHandler(score, suggestions);
+		
 		}
 	}
 
@@ -111,14 +111,14 @@ export default function App() {
 		console.log("update state", score)
 	}, [score]);
 
-	const adviceHandler = (score) => {
+	const adviceHandler = (score, suggestions) => {
 		console.log("score in handler is " + score);
 		if (score > 80) {
-			setAdvice(`Congratulations! It looks like you are one of those people whose work and lifestyle habits don't make you too susceptible to skin cancer! It is very rare for you to have such a good working environment in rural Victoria where the incidence of skin cancer is very high. Congratulations and hope you will stay free of skin cancer ever! However, here are a few suggestions for your work and living environment and habits that we hope you will read carefully and use. This way, you will have a good chance of keeping your skin healthy!`);
+			setAdvice(`Congratulations! It looks like you are one of those people whose work and lifestyle habits don't make you too susceptible to skin cancer!` + suggestions)
 		} else if (score > 40 && score <= 80) {
-			setAdvice(`Ah-oh! It looks like you have some habits and work environments that expose you to the threat of UV rays. While it may not seem like such a threat is serious right now, you do need to be fully aware of the importance of sun protection. After all, staying healthy is extremely critical to you and your families! Here are a few suggestions for your work and living environment and habits that we hope you will read carefully and use. This way, you will have a good chance of keeping your skin healthy!`);
+			setAdvice(`Ah-oh! It looks like you have some habits and work environments that expose you to the threat of UV rays.` + suggestions);
 		} else {
-			setAdvice(`Ooops! It looks like your work environment and lifestyle habits expose you to the threat of UV rays on a regular basis! Such a threat can put you at a much higher risk of developing skin cancer. But don't worry, we'll give you a series of tips that, if you really take them seriously and keep them up, you'll be well away from the risk of developing the disease. Take a look at the advice below! Do what you have to do to maintain healthy skin in the future.`);
+			setAdvice(`Ooops! It looks like your work environment and lifestyle habits expose you to the threat of UV rays on a regular basis!` + suggestions);
 		}
 	}
 
@@ -148,7 +148,7 @@ export default function App() {
 					</div>
 					<div className='answer-section'>
 						{questions[currentQuestion].answerOptions.map((answerOption, index) => (
-							<button onClick={() => {handleAnswerButtonClick(answerOption.point); console.log("the score onClick is " + answerOption.point);}}>{answerOption.answerText}</button>
+							<button onClick={() => {handleAnswerButtonClick(answerOption.point, answerOption.tips); console.log("the score onClick is " + answerOption.point);}}>{answerOption.answerText}</button>
 						))}
 					</div>
 					
